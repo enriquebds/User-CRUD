b586e9377dcc03345a1112633353389ba74034a6
Enrique <enrique.barbosasilva@gmail.com>
2022-09-27 19:36:25 -0300

    feat: route-of-get-users-created

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import createUserController from "../controllers/createUser.controller";
+import listUsersController from "../controllers/listUser.controller";
 
-const routes = Router()
+const routes = Router();
 
-routes.post("", createUserController)
+routes.post("", createUserController);
+routes.get("", listUsersController);
 
-export default routes
+export default routes;
