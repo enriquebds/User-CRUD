d10b2fb9019a58a022906b6d8ca6a9232eded831
Enrique <enrique.barbosasilva@gmail.com>
2022-09-27 19:54:39 -0300

    refactor: alterations-in-app-archive

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
-import express from "express"
-import userRoutes from "./routes/user.routes"
+import express from "express";
+import userRoutes from "./routes/user.routes";
+import loginRoutes from "./routes/login.routes";
 
-const app = express()
-app.use(express.json())
-app.use("/user", userRoutes)
+const app = express();
+app.use(express.json());
+app.use("/user", userRoutes);
+app.use("/login", loginRoutes);
 
 app.listen(3000, () => {
-    console.log("Listen in port 3000")
-})
+  console.log("Listen in port 3000");
+});
