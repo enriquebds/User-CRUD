535a69ae4dc50ed32382d5d0f2f0da5080bb6306
Enrique <enrique.barbosasilva@gmail.com>
2022-10-01 00:53:19 -0300

    feat: route-of-delete-and-patch-created

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,12 +1,17 @@
 import { Router } from "express";
 import createUserController from "../controllers/createUser.controller";
+import deleteUserController from "../controllers/deleteUser.controller";
 import listUsersController from "../controllers/listUser.controller";
 import listUserByIdController from "../controllers/listUserById.controller";
+import updatedUserController from "../controllers/updateUser.controller";
+import verifyAuthToken from "../middlewares/verifyAuthToken.middleware";
 
 const routes = Router();
 
 routes.post("", createUserController);
-routes.get("", listUsersController);
+routes.get("", verifyAuthToken, listUsersController);
 routes.get("/:uuid", listUserByIdController);
+routes.patch("/:uuid", verifyAuthToken, updatedUserController);
+routes.get("/:uuid", verifyAuthToken, deleteUserController);
 
 export default routes;
