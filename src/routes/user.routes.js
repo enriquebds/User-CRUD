6d958e94486105dd4a418060cd250ab52e3fbc57
Enrique <enrique.barbosasilva@gmail.com>
2022-10-01 00:42:08 -0300

    feat: routes-of-listUserById-created

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import createUserController from "../controllers/createUser.controller";
 import listUsersController from "../controllers/listUser.controller";
+import listUserByIdController from "../controllers/listUserById.controller";
 
 const routes = Router();
 
 routes.post("", createUserController);
 routes.get("", listUsersController);
+routes.get("/:uuid", listUserByIdController);
 
 export default routes;
