ac995e91cbab6e9d63df48e9aad1b1bafd954ec1
Enrique <enrique.barbosasilva@gmail.com>
2022-10-03 17:47:51 -0300

    feat: user-serializer-creatd

--- src/serializers/user.serializer.js
+++ src/serializers/user.serializer.js
@@ -1,5 +1,12 @@
 import * as yup from "yup";
 
-const createUserSerializer = yup.object().shape({});
+const createUserSerializer = yup.object().shape({
+  uuid: yup.string().notRequired(),
+  createdOn: yup.string().notRequired(),
+  updatedOn: yup.string().notRequired(),
+  email: yup.string().email().required(),
+  name: yup.string().required(),
+  password: yup.string().required(),
+});
 
 export { createUserSerializer };
