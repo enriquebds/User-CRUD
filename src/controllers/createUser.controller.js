6bbb06f34ac2b352abe0e3a20e2122eaca010eb6
Enrique <enrique.barbosasilva@gmail.com>
2022-10-03 17:44:10 -0300

    refactor: code-changed

--- src/controllers/createUser.controller.js
+++ src/controllers/createUser.controller.js
@@ -1,11 +1,17 @@
 import createUser from "../services/createUser";
 
-const createUserController = async (req, res) => {    
-    const {name, email, password} = req.body
+const createUserController = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
 
-    const user = await createUser(name, email, password)
+    const user = await createUser(name, email, password);
 
-    return res.status(201).json(user)
-}
+    return res.status(201).json(user);
+  } catch (error) {
+    return res.status(400).json({
+      error: error.errors,
+    });
+  }
+};
 
-export default createUserController
+export default createUserController;
