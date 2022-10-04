aa5f3682fbbdc2376ffb83ece52ae506977062bf
Enrique <enrique.barbosasilva@gmail.com>
2022-10-04 12:25:18 -0300

    refactor: code-changed

--- src/controllers/createUser.controller.js
+++ src/controllers/createUser.controller.js
@@ -1,15 +1,14 @@
 import createUser from "../services/createUser";
 
 const createUserController = async (req, res) => {
+  const { name, email, password } = req.body;
   try {
-    const { name, email, password } = req.body;
-
     const user = await createUser(name, email, password);
 
     return res.status(201).json(user);
   } catch (error) {
     return res.status(400).json({
-      error: error.errors,
+      message: error.message,
     });
   }
 };
--- src/controllers/listUser.controller.js
+++ src/controllers/listUser.controller.js
@@ -1,10 +1,16 @@
 import users from "../database/users";
 import listUsers from "../services/listUsers";
 
-const listUsersController = (req, res) => {
-  const user = listUsers(users);
+const listUsersController = async (req, res) => {
+  try {
+    const user = await listUsers(users);
 
-  return res.status(200).json(user);
+    return res.status(200).json(user);
+  } catch (error) {
+    return res.status(400).json({
+      message: error.message,
+    });
+  }
 };
 
 export default listUsersController;
