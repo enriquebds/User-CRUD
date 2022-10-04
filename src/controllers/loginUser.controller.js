0707bf8c05dc68f0054f559a96c4e411d3bf6eb6
Enrique <enrique.barbosasilva@gmail.com>
2022-10-04 14:28:35 -0300

    refactor: code-changed

--- src/controllers/loginUser.controller.js
+++ src/controllers/loginUser.controller.js
@@ -1,11 +1,17 @@
 import loginUser from "../services/loginUser";
 
-const loginUserController = (req, res) => {
+const loginUserController = async (req, res) => {
   const { email, password } = req.body;
 
-  const login = loginUser(email, password);
+  try {
+    const login = await loginUser(email, password);
 
-  return res.status(200).json(login);
+    return res.status(200).json(login);
+  } catch (error) {
+    return res.status(400).json({
+      message: error.message,
+    });
+  }
 };
 
 export default loginUserController;
