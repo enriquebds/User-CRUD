a989612d40cb05453485ec5c64a0d11b82e02188
Enrique <enrique.barbosasilva@gmail.com>
2022-10-05 18:10:15 -0300

    refactor: code-changed

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import userRoutes from "./routes/user.routes";
 import loginRoutes from "./routes/login.routes";
 import petsRouter from "./routes/pets.routes";
 import { startDatabase } from "./database/users";
+import "dotenv/config";
 
 const app = express();
 app.use(express.json());
