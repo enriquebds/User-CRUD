92b0ee8b2f1cdbb05675848b0ef87e9691124666
Enrique <enrique.barbosasilva@gmail.com>
2022-10-05 16:58:48 -0300

    refactor: code-changed

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 import express from "express";
 import userRoutes from "./routes/user.routes";
 import loginRoutes from "./routes/login.routes";
+import petsRouter from "./routes/pets.routes";
 import { startDatabase } from "./database/users";
 
 const app = express();
 app.use(express.json());
 app.use("/user", userRoutes);
 app.use("/login", loginRoutes);
+app.use("/pets", petsRouter);
 
 app.listen(3000, () => {
   startDatabase();
