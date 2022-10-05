81737071e3bf51f203d85e98dc06f93c48bb2933
Enrique <enrique.barbosasilva@gmail.com>
2022-10-05 18:09:44 -0300

    refactor: listUserPets-get-route-created

--- src/routes/pets.routes.js
+++ src/routes/pets.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import createPetController from "../controllers/createPet.controller";
+import listUsersPetsController from "../controllers/listUsersPets.controller";
 import verifyAuthToken from "../middlewares/verifyAuthToken.middleware";
 
 const routes = Router();
 
 routes.post("/:ownder_id/new", verifyAuthToken, createPetController);
+routes.get("/:ownder_id", verifyAuthToken, listUsersPetsController);
 
 export default routes;
